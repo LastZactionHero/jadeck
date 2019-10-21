cae3efab99c8320f464d0d2d89b2037069157cf3
Zach Dicklin <zdicklin@gmail.com>
2019-10-21 15:51:55 +0000

    First commit

--- src/main/java/org/eclipse/che/examples/Deck.java
+++ src/main/java/org/eclipse/che/examples/Deck.java
@@ -2,22 +2,11 @@
  *  Copyright (c) Red Hat, Inc. All rights reserved.
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
-package com.codenvy.example.java;
+package org.eclipse.che.examples;
 
-import static org.junit.Assert.assertTrue;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
-
-/**
- * Unit test for simple App.
- */
-@RunWith(JUnit4.class)
-public class AppTest {
-
-    @Test
-    public void testApp() {
-        assertTrue(true);
+public class Deck {
+    public static void main(String... argvs) {
+        String a = "Che";
+        System.out.println("Hello World " + a + "!");
     }
 }
---
+++ src/test/java/com/codenvy/example/java/DeckTest.java
@@ -0,0 +1,53 @@
+/*
+ *  Copyright (c) Red Hat, Inc. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *————————————————————————————————————————————*/
+package com.codenvy.example.java;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.Type;
+import java.nio.charset.Charset;
+import java.util.Collection;
+
+import org.apache.commons.io.IOUtils;
+import org.eclipse.che.examples.Deck;
+import org.eclipse.che.examples.Term;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import static org.junit.Assert.*;
+
+/**
+ * Unit test for simple App.
+ */
+@RunWith(JUnit4.class)
+
+public class DeckTest {
+    
+    @Test
+    public void testApp() {        
+        Type collectionType = new TypeToken<Collection<Term>>(){}.getType();
+        
+        Gson g = new Gson();
+
+        InputStream is = getClass().getClassLoader().getResourceAsStream("deck.json");
+        String json = null;
+        try {
+            json = IOUtils.toString(is, Charset.forName("UTF-8"));
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        Collection<Term> terms = g.fromJson(json, collectionType);
+        Term t = (Term) terms.toArray()[0];
+        assertEquals(t.uuid, "f643015f-2e99-4636-853a-24e971bf9a85");
+    }
+}

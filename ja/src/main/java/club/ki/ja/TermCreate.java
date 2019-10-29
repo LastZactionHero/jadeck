99dd46906e102f4ff0d6d7eccadcc9f19d096e26
Zach <zdicklin@gmail.com>
2019-10-29 16:19:25 +0000

    Whole new project!

---
+++ ja/src/main/java/club/ki/ja/TermCreate.java
@@ -0,0 +1,18 @@
+package club.ki.ja;
+
+import java.io.IOException;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name="TermCreate", value="/terms")
+public class TermCreate extends HttpServlet {
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+        throws IOException {
+        response.setContentType("application/json");
+        response.getWriter().println("Term Create");
+    }
+}
